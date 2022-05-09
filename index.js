f52bf05a080cc57a7df29e0e74ceb87b7c0dba97
Nicholas <mullally.nt@gmail.com>
2022-05-08 18:42:37 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+
+//Fetch
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: "Bob",
+      email: "Bob@gmail.comgkii8",
+    }),
+  };
+  
+fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    });
+
+fetch("http://localhost:3000/users", configurationObject);
+const formData = {
+    name: "Bob",
+    email: "Bob@gmail.com",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  
